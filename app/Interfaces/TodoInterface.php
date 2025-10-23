ff969e032b90f62227a992da1f8e179e5aac7f73
JRyan-123 <acostajoseryan4@gmail.com>
2025-10-23 17:44:36 +0800

    interface and repo bind

---
+++ app/Interfaces/TodoInterface.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Interfaces;
+
+class TodoInterface
+{
+    /**
+     * Create a new class instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+}
---
+++ app/Repositories/TodoRepository.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Repositories;
+
+class TodoRepository
+{
+    /**
+     * Create a new class instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+}
